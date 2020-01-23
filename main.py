77d633455540f0691998648a2d675b7d8e0acb60
Porya <gohary.porya@gmail.com>
2020-01-23 11:43:17 +0330

    Adding Task Drawing

--- main.py
+++ main.py
@@ -1,41 +1,68 @@
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from dataclasses import dataclass
+
+core = 4  # number of Cores
+deadline = 100  # Deadline
+Max_freq = 2  # Max Freq. in GHz
+
+Task_color='darkgrey'
 
 
-core=4 #number of Cores
-deadline =100
-T1 = [10, 2]  # Task specification  [ Run Time , Freq]
+@dataclass
+class Task:
+    # Task specification  [ Name, WCET , Freq, Core, Start Time]
+    name: str = 'none'
+    Wcet: int = 0
+    Freq: float = 0
+    core: int = 0
+    start: int = 0
 
 
-bold_text = {'ha': 'center', 'va': 'center', 'family': 'sans-serif','fontweight': 'bold'}
-regular_text = {'ha': 'center', 'va': 'center', 'family': 'sans-serif','fontweight': 'regular'}
+# T = Task('T2', 10, 2, 1, 10)
+
+T = [Task() for _ in range(3)]
+
+T[0] = Task('T1', 20, 1.6, 2, 30)
+T[1] = Task('T2', 10, 2, 1, 10)
+T[2] = Task('T3', 20, 1.2, 1, 20)
+
+bold_text = {'ha': 'center', 'va': 'center', 'family': 'sans-serif', 'fontweight': 'bold'}
+regular_text = {'ha': 'center', 'va': 'center', 'family': 'sans-serif', 'fontweight': 'regular'}
 fig = plt.figure()
-#figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+# figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
 ax = fig.add_subplot(111)
-rect1 = matplotlib.patches.Rectangle((0, 1), 5, 0.5, color='c')
 
-ax.add_patch(rect1)
 # ax.grid()
 
-plt.xlim([-10, deadline+10])
-plt.ylim([0, 20*core])
+plt.xlim([-10, deadline + 10])
+plt.ylim([-10, 20 * core])
 
-#Draw Deadline
-x = [deadline,deadline]
-y = [0,20*core]
-plt.plot(x,y,'--', color='r')
-plt.text(deadline, 20*core+1, 'Deadline', color='r', size=10,**regular_text)
+# Draw Deadline
+x = [deadline, deadline]
+y = [0, 20 * core]
+plt.plot(x, y, '--', color='r')
+plt.text(deadline, 20 * core + 1, 'Deadline', color='r', size=11, **regular_text)
 ###########
 
-#write Cores Name
-for i in range(1, core+1):
-    plt.text(-5, (20*core+10)-(20*i), 'Core '+str(i), color='black', size=10,**regular_text)
+# write Cores Name and Draw Axes for each core
+for i in range(1, core + 1):
+    plt.text(-6, 8 + (20 * (i - 1)), 'Core ' + str(i), color='black', size=12, **regular_text)
+    ax.arrow(0, (20 * (i - 1)), deadline + 2, 0, head_width=1, head_length=1, fc='k', ec='k')
+    ax.arrow(0, (20 * (i - 1)), 0, 15, head_width=1, head_length=1, fc='k', ec='k')
 #####
 
+# Write Time under Axes
+for i in range(0, deadline + 10, 10):
+    plt.text(i, -2, str(i), color='black', size=10, **regular_text)
+######
 
-#ax = plt.axes()
-ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
+for x in T:
+    rect1 = Rectangle((x.start, (x.core - 1) * 20), x.Wcet, 12 * x.Freq / Max_freq, facecolor=Task_color, edgecolor='black')
+    ax.add_patch(rect1)
+    plt.text(x.start + (x.Wcet / 2), (20 * (x.core - 1)) + 6 * x.Freq / Max_freq, x.name, color='black', size=12,
+             **regular_text)
 
 plt.axis('off')
 plt.show()
